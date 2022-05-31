afb61a0d215eb19e03b5c5f94a6e22645fabdb68
Swarm-J <julianschinkel@gmail.com>
2022-05-31 16:25:59 +0200

    assignment strings

---
+++ print/main.py
@@ -0,0 +1,16 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+numbers = [x for x in range(31)]
+print(numbers)
+
+even_numbers = [x for x in range(31) if x % 2 == 0]
+print(even_numbers)
+
+text = "These are some words for you"
+print(text)
+
+backwards_text = text[::-1]
+print(backwards_text)
