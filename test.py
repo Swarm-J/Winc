8264b1d76da2ef98facdb68582f93653782cad7a
Swarm-J <julianschinkel@gmail.com>
2022-06-02 15:04:14 +0200

    updated exercises

--- test.py
+++ test.py
@@ -1,32 +1,8 @@
-# Add your code after this line
-# Players that scored
-scorer_1 = "Ruud Gullit"
-scorer_2 = "Marco van Basten"
+country = 'South Georgia and the South Sandwich Islands'
+vowels = 'aeiou'
+count = 0
+for c in country:
+    if c in vowels:
+        count += 1
 
-# Goals Scored during the match in minutes
-goal_0 = 32
-goal_1 = 54
-
-scorers = scorer_1 + ' ' + str(goal_0) + ', ' + scorer_2 + ' ' + str(goal_1)
-print(scorers)
-
-report = f"{scorer_1} scored in the {goal_0}nd minute\n{scorer_2} scored in the {goal_1}th minute"
-print(report)
-
-player = "Frank Rijkaard"
-
-first_name = player[player.find("Frank"):5]
-print(first_name)
-
-last_name_len = len(player[player.find("Rijkaard"):])
-print(last_name_len)
-
-name_short = f"{player[0]}. {player[player.find('Rijkaard'):]}"
-print(name_short)
-
-full_chant = (f'{player[player.find("Frank"):5]}! ') * len(player[player.find("Frank"):5])
-chant = full_chant[:-1]
-print(chant)
-
-good_chant = chant[len(chant) - 1] != ' '
-print(good_chant)
+print(count)
